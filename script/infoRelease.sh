dcaaf407d524f952e30ae00eedf289c37f3a68c0
kiereleaseuser <kiereleaseuser@gmail.com>
2015-03-12 13:24:32 +0100

    sync

--- script/infoRelease.sh
+++ script/infoRelease.sh
@@ -10,29 +10,32 @@ cd ../reports/tags
 fileDir=$(pwd)
 
 
-if [ $# != 2 ] ; then
+if [ $# != 4 ] ; then
     echo
     echo "Usage:"
-    echo "  $0 communityTag productTag"
+    echo "  $0 communityTag productTag targetProdBuild cutOffDate"
     echo "For example:"
-    echo "  $0 6.2.0.Final sync-6.2.x-2015.02.27"
+    echo "  $0 6.2.0.Final sync-6.2.x-2015.02.27 ER6 2015-02-27"
     echo
     exit 1
 fi
 
 echo "The communityTag is: "$1
 echo "The prodcutTag is: "$2
+echo "The target product build is: "$3
+echo "The cutoff date is: " $4
 echo -n "Is this ok? (Hit control-c if is not): "
 read ok
 
 
 communityTag=$1
 productTag=$2
-#prefix=`date +%F-%H:%M`
+targetProdBuild=$3
+cutOffDate=$4
 
 #checks if $productTag is already existing in a filename
 cd $fileDir
-LIST=$(find . -name "$productTag*.txt")
+LIST=$(find . -name "$productTag*.txt" | sort)
 
 # all filename containing $productTag are listed. The filename with the highest *.txt is listed as last.
 # The LIST is reverted and the value * extracted
@@ -49,20 +52,39 @@ fi
 # name of file to be written and pushed  
 fileToWrite=$productTag-$counter.txt
 
+
 cd $scriptDir
-pwd
 
 CONTACTS=$(cat mails.properties)
-REPOSITORIES=$(cat repositories.properties)
+# add to URL in repository.properties /tree/$communityTag
+FILE_TO_READ=$scriptDir/repositories.properties
+   while read line; do
+     if [ -n "$line" ]; then
+       echo "$line"/tree/"$communityTag" >> repURLS.txt
+     fi
+   done < $FILE_TO_READ
+REPOSITORIES=$(cat repURLS.txt)
+rm repURLS.txt
 MAVEN=$(mvn -version)
 NOTES=$(cat notes.properties)
 # JAVA version as it neds a workaround
 java -version 2>>javaVersion.txt
 JAVAV=$(cat javaVersion.txt)
+DATETIME=`date +%F-%H:%M`
 
 
 cat <<EOF >$fileToWrite
+
+------------------------------------------------------------------------
+           Engineering to Productization Handoff Report
+------------------------------------------------------------------------
+Report Date: $DATETIME
+Code Cutoff Date: $cutOffDate
+Target Product Build: $targetProdBuild
+Source Product Tag: $productTag
+Community Tag (if available): $communityTag
  
+
 ------------------------------------------------------------------------   
                      Component owner contacts                           
 ------------------------------------------------------------------------
@@ -78,10 +100,10 @@ $REPOSITORIES
 ------------------------------------------------------------------------
                         Software versions                               
 ------------------------------------------------------------------------
-$JAVAV
+JAVA: $JAVAV
 ________________________________________________________________________
 
-$MAVEN
+MAVEN: $MAVEN
 
 
 ------------------------------------------------------------------------
@@ -95,8 +117,9 @@ EOF
 
 # pushes $fileToWrite to the blessed repository
    rm javaVersion.txt
-   mv $fileToWrite $fileDir/ .
-   git add .
+   mv $fileToWrite $fileDir/
+   cd $fileDir
+   git add $fileToWrite
    git commit -m "$productTag"
    git push origin master
    
